91e66d163fc1b743868d66fc4308e2ec14156ad6
Ege Gurmericliler <egurmeri@gmail.com>
2020-06-18 22:15:42 -0400

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
-echo "Setting up revtrvp."
-
-/revtrvp $@
-
-echo "Starting traffic listener."
+echo "Setting up revtrvp and starting the traffic listener."
 
 # Setup a cron schedule
 echo "* * * * * bash /traffic_monitoring/traffic_listener_cron.sh >> /var/log/cron.log 2>&1
 # Don't remove the empty line at the end of this file. It is required to run the cron job" > /traffic_monitoring/traffic_crontab
 
 crontab /traffic_monitoring/traffic_crontab
-cron -f
+
+/revtrvp $@ && cron -f
